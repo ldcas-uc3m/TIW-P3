4e3096b18bec0a3eea75ee3190cb7c9263579c6b
ldcas-uc3m <78721925+ldcas-uc3m@users.noreply.github.com>
2023-12-11 19:26:21 +0100

    small front touches

--- front/src/main/java/es/uc3m/tiw/controllers/MainController.java
+++ front/src/main/java/es/uc3m/tiw/controllers/MainController.java
@@ -1,39 +1,31 @@
 package es.uc3m.tiw.controllers;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.MediaType;
-
 
 import es.uc3m.tiw.entities.Equipo;
 import es.uc3m.tiw.entities.Jugador;
 import es.uc3m.tiw.entities.Plantilla;
 import es.uc3m.tiw.entities.PlantillaKey;
 import es.uc3m.tiw.entities.Posicion;
+import es.uc3m.tiw.entities.Usuario;
+
 
 
 @RestController
-@CrossOrigin
 public class MainController {
 
+	@Autowired
+	RestTemplate restTemplate;
 
     /* REDIRECCIONES */
 
     @GetMapping("/formNewJugador")
     public String formNewJugador() {
-        return "formNewJugador"; // Devuelve el nombre de la plantilla Thymeleaf (sin extensión .html)
+        return "formNewJugador";  // Devuelve el nombre de la plantilla Thymeleaf (sin extensión .html)
     }
 
     @GetMapping("/formUpgradeJugador")
@@ -59,74 +51,74 @@ public class MainController {
 
 
 	/* NAVEGACION */
-	@RequestMapping("/")
-	public String index(){
+	@GetMapping("/")
+	public String index() {
 		return "index";
 	}
 
-	@RequestMapping (value = "pagina-crear-usuario", method = RequestMethod.GET)
+	@GetMapping ("pagina-crear-usuario")
 	public String mostrarElFormularioDelUsuario(Model modelo){
-		modelo.addAttribute("usuario", new User());
+		modelo.addAttribute("usuario", new Usuario());
 		return "ViewCrearUsuario.html";
 	}
 
-	@RequestMapping (value = "pagina-borrar-usuario", method = RequestMethod.GET)
-	public String mostrarElFormularioBorrarUsuario(){
+	@GetMapping ("pagina-borrar-usuario")
+	public String mostrarElFormularioBorrarUsuario() {
 		return "ViewDeleteUsuario.html";
 	}
 
-	@RequestMapping (value = "pagina-update-usuario", method = RequestMethod.GET)
-	public String mostrarElFormularioUpdateUsuario(Model modelo){
-		modelo.addAttribute("usuario", new User());
+	@GetMapping ("pagina-update-usuario")
+	public String mostrarElFormularioUpdateUsuario(Model modelo) {
+		modelo.addAttribute("usuario", new Usuario());
 		return "ViewUpdateUsuario.html";
 	}
 
 
 	/* LLAMADAS AL CONTROLADOR */
-	@RequestMapping (value = "pagina-usuario/{name}", method = RequestMethod.GET)
+	@GetMapping ("pagina-usuario/{name}")
 	public String returnUsuarios(Model model, @PathVariable String name) {
 
-		User us = restTemplate.getForObject("http://localhost:8082/user/{name}", User.class, name);
+		Usuario us = restTemplate.getForObject("http://localhost:8082/user/{name}", Usuario.class, name);
 		model.addAttribute("usuario", us);
 		return "viewUsuarios";
 
 	}
 
-	@RequestMapping (value = "pagina-todos-usuarios", method = RequestMethod.GET)
+	@GetMapping ("pagina-todos-usuarios")
 	public String returnTodosUsuarios(Model model) {
-		User[] listaUs = restTemplate.getForObject("http://localhost:8082/users", User[].class);
+		Usuario[] listaUs = restTemplate.getForObject("http://localhost:8082/users", Usuario[].class);
 		model.addAttribute("userList", listaUs);
 		return "viewTodosUsuarios";
 	}
 
 
-	@RequestMapping (value = "pagina-post-usuario", method = RequestMethod.POST)
-	public String saveUser(Model model, @ModelAttribute User us) {
-		User newUser = restTemplate.postForObject("http://localhost:8082/users", us, User.class);
+	@PostMapping ("pagina-post-usuario")
+	public String saveUser(Model model, @ModelAttribute Usuario us) {
+		Usuario newUser = restTemplate.postForObject("http://localhost:8082/users", us, Usuario.class);
 		model.addAttribute("usuario", newUser);
 		return "viewUsuarios";
 	}
 
-	@RequestMapping (value = "pagina-delete-usuario", method = RequestMethod.POST)
-	public String deleteUser(Model model, @RequestParam String userName){
-		User delUser = restTemplate.getForObject("http://localhost:8082/user/{name}", User.class, userName);
+	@PostMapping ("pagina-delete-usuario")
+	public String deleteUser(Model model, @RequestParam String userName) {
+		Usuario delUser = restTemplate.getForObject("http://localhost:8082/user/{name}", Usuario.class, userName);
 		if (delUser != null) {
-			restTemplate.delete("http://localhost:8082/users/{id}", delUser.getIduser());
+			restTemplate.delete("http://localhost:8082/users/{id}", delUser.getUserCorreo());
 		}
 		return "index";
 	}
 
-	@RequestMapping (value = "pagina-search-usuario", method = RequestMethod.POST)
+	@PostMapping ("pagina-search-usuario")
 	public String searchUsuarios(Model model, @RequestParam String name) {
-		User us = restTemplate.getForObject("http://localhost:8082/user/{name}", User.class, name);
+		Usuario us = restTemplate.getForObject("http://localhost:8082/user/{name}", Usuario.class, name);
 		model.addAttribute("usuario", us);
 		return "viewUpdateUsuario";
 
 	}
 
-	@RequestMapping (value = "pagina-update-usuario", method = RequestMethod.POST)
-	public String deleteUser(Model model, @ModelAttribute User us){
-		restTemplate.put("http://localhost:8082/users", us, User.class);
+	@PostMapping ("pagina-update-usuario")
+	public String deleteUser(Model model, @ModelAttribute Usuario us){
+		restTemplate.put("http://localhost:8082/users", us, Usuario.class);
 		return "index";
 	}
 
