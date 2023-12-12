97dd1d515bb0ff3429142d6cf28154e93f621111
iBertXx <100429046@alumnos.uc3m.es>
2023-12-12 17:40:13 +0100

    Update MainController.java
    crud admin y cambios en redirecciones y puertos

--- front/src/main/java/es/uc3m/tiw/controllers/MainController.java
+++ front/src/main/java/es/uc3m/tiw/controllers/MainController.java
@@ -12,6 +12,7 @@ import es.uc3m.tiw.entities.Plantilla;
 import es.uc3m.tiw.entities.PlantillaKey;
 import es.uc3m.tiw.entities.Posicion;
 import es.uc3m.tiw.entities.Usuario;
+import es.uc3m.tiw.entities.Administrador;
 
 
 
@@ -74,11 +75,11 @@ public class MainController {
 	}
 
 
-	/* LLAMADAS AL CONTROLADOR - USUARIOS */
-	@GetMapping ("pagina-usuario/{name}")
-	public String returnUsuarios(Model model, @PathVariable String name) {
+	/* CRUD CONTROLADOR - USUARIOS */
+	@GetMapping ("pagina-usuario/{nombre}")
+	public String returnUsuarios(Model model, @PathVariable String nombre) {
 
-		Usuario us = restTemplate.getForObject("http://localhost:8082/user/{name}", Usuario.class, name);
+		Usuario us = restTemplate.getForObject("http://localhost:8021/user/{nombre}", Usuario.class, nombre);
 		model.addAttribute("usuario", us);
 		return "viewUsuarios";
 
@@ -86,7 +87,7 @@ public class MainController {
 
 	@GetMapping ("pagina-todos-usuarios")
 	public String returnTodosUsuarios(Model model) {
-		Usuario[] listaUs = restTemplate.getForObject("http://localhost:8082/users", Usuario[].class);
+		Usuario[] listaUs = restTemplate.getForObject("http://localhost:8021/users", Usuario[].class);
 		model.addAttribute("userList", listaUs);
 		return "viewTodosUsuarios";
 	}
@@ -94,23 +95,23 @@ public class MainController {
 
 	@PostMapping ("pagina-post-usuario")
 	public String saveUser(Model model, @ModelAttribute Usuario us) {
-		Usuario newUser = restTemplate.postForObject("http://localhost:8082/users", us, Usuario.class);
+		Usuario newUser = restTemplate.postForObject("http://localhost:8021/users", us, Usuario.class);
 		model.addAttribute("usuario", newUser);
 		return "viewUsuarios";
 	}
 
 	@PostMapping ("pagina-delete-usuario")
 	public String deleteUser(Model model, @RequestParam String userName) {
-		Usuario delUser = restTemplate.getForObject("http://localhost:8082/user/{name}", Usuario.class, userName);
+		Usuario delUser = restTemplate.getForObject("http://localhost:8021/user/{nombre}", Usuario.class, userName);
 		if (delUser != null) {
-			restTemplate.delete("http://localhost:8082/users/{id}", delUser.getUserCorreo());
+			restTemplate.delete("http://localhost:8082/users/{nombre}", delUser.getUserCorreo());
 		}
 		return "index";
 	}
 
 	@PostMapping ("pagina-search-usuario")
-	public String searchUsuarios(Model model, @RequestParam String name) {
-		Usuario us = restTemplate.getForObject("http://localhost:8082/user/{name}", Usuario.class, name);
+	public String searchUsuarios(Model model, @RequestParam String nombre) {
+		Usuario us = restTemplate.getForObject("http://localhost:8021/user/{nombre}", Usuario.class, nombre);
 		model.addAttribute("usuario", us);
 		return "viewUpdateUsuario";
 
@@ -123,11 +124,11 @@ public class MainController {
 	}
 
 
-	/* LLAMADAS AL CONTROLADOR - EQUIPOS */
-	@GetMapping ("pagina-equipo/{name}")
-	public String returnEquipos(Model model, @PathVariable String name) {
+	/* CRUD CONTROLADOR - EQUIPOS */
+	@GetMapping ("pagina-equipo/{nombre}")
+	public String returnEquipos(Model model, @PathVariable String nombre) {
 
-		Equipo eq = restTemplate.getForObject("http://localhost:8082/equipo/{name}", Equipo.class, name);
+		Equipo eq = restTemplate.getForObject("http://localhost:8022/equipo/{nombre}", Equipo.class, nombre);
 		model.addAttribute("equipo", eq);
 		return "viewEquipos";
 
@@ -135,30 +136,30 @@ public class MainController {
 
 	@GetMapping ("pagina-todos-equipos")
 	public String returnTodosEquipos(Model model) {
-		Equipo[] listaEq = restTemplate.getForObject("http://localhost:8082/equipos", Equipo[].class);
+		Equipo[] listaEq = restTemplate.getForObject("http://localhost:8022/equipos", Equipo[].class);
 		model.addAttribute("equipoList", listaEq);
 		return "viewTodosEquipos";
 	}
 
 	@PostMapping ("pagina-post-equipo")
 	public String saveEquipo(Model model, @ModelAttribute Equipo eq) {
-		Equipo newEquipo = restTemplate.postForObject("http://localhost:8082/equipos", eq, Equipo.class);
+		Equipo newEquipo = restTemplate.postForObject("http://localhost:8022/equipos", eq, Equipo.class);
 		model.addAttribute("equipo", newEquipo);
 		return "viewEquipos";
 	}
 
 	@PostMapping ("pagina-delete-equipo")
 	public String deleteEquipo(Model model, @RequestParam String equipoName) {
-		Equipo delEquipo = restTemplate.getForObject("http://localhost:8082/equipos/{name}", Equipo.class, equipoName);
+		Equipo delEquipo = restTemplate.getForObject("http://localhost:8022/equipos/{nombre}", Equipo.class, equipoName);
 		if (delEquipo != null) {
-			restTemplate.delete("http://localhost:8082/equipos/{id}", delEquipo.getNombre());
+			restTemplate.delete("http://localhost:8082/equipos/{nombre}", delEquipo.getNombre());
 		}
 		return "index";
 	}
 
 	@PostMapping ("pagina-search-equipo")
-	public String searchEquipos(Model model, @RequestParam String name) {
-		Equipo eq = restTemplate.getForObject("http://localhost:8082/equipos/{name}", Equipo.class, name);
+	public String searchEquipos(Model model, @RequestParam String nombre) {
+		Equipo eq = restTemplate.getForObject("http://localhost:8022/equipos/{nombre}", Equipo.class, nombre);
 		model.addAttribute("equipo", eq);
 		return "viewUpdateEquipo";
 
@@ -166,16 +167,16 @@ public class MainController {
 
 	@PostMapping ("pagina-update-equipo")
 	public String deleteEquipo(Model model, @ModelAttribute Equipo eq){
-		restTemplate.put("http://localhost:8082/equipos", eq, Equipo.class);
+		restTemplate.put("http://localhost:8022/equipos", eq, Equipo.class);
 		return "index";
 	}
 
 
-	/* LLAMADAS AL CONTROLADOR - JUGADORES */
+	/* CRUD CONTROLADOR - JUGADORES */
 	@GetMapping ("pagina-jugador/{dni}")
 	public String returnJugadores(Model model, @PathVariable String dni) {
 
-		Jugador ju = restTemplate.getForObject("http://localhost:8082/jugador/{dni}", Jugador.class, dni);
+		Jugador ju = restTemplate.getForObject("http://localhost:8022/jugador/{dni}", Jugador.class, dni);
 		model.addAttribute("jugador", ju);
 		return "viewJugadores";
 
@@ -183,7 +184,7 @@ public class MainController {
 
 	@GetMapping ("pagina-todos-jugadores")
 	public String returnTodosJugadores(Model model) {
-		Jugador[] listaJu = restTemplate.getForObject("http://localhost:8082/jugadores", Jugador[].class);
+		Jugador[] listaJu = restTemplate.getForObject("http://localhost:8022/jugadores", Jugador[].class);
 		model.addAttribute("jugadorList", listaJu);
 		return "viewTodosJugadores";
 	}
@@ -191,14 +192,14 @@ public class MainController {
 
 	@PostMapping ("pagina-post-jugador")
 	public String saveJugador(Model model, @ModelAttribute Jugador ju) {
-		Jugador newJugador = restTemplate.postForObject("http://localhost:8082/jugadores", ju, Jugador.class);
+		Jugador newJugador = restTemplate.postForObject("http://localhost:8022/jugadores", ju, Jugador.class);
 		model.addAttribute("jugador", newJugador);
 		return "viewJugadores";
 	}
 
 	@PostMapping ("pagina-delete-jugador")
 	public String deleteJugador(Model model, @RequestParam String jugadorDNI) {
-		Jugador delJugador = restTemplate.getForObject("http://localhost:8082/jugador/{dni}", Jugador.class, jugadorDNI);
+		Jugador delJugador = restTemplate.getForObject("http://localhost:8022/jugador/{dni}", Jugador.class, jugadorDNI);
 		if (delJugador != null) {
 			restTemplate.delete("http://localhost:8082/jugadores/{dni}", delJugador.getDni());
 		}
@@ -207,7 +208,7 @@ public class MainController {
 
 	@PostMapping ("pagina-search-jugador")
 	public String searchJugadores(Model model, @RequestParam String dni) {
-		Jugador ju = restTemplate.getForObject("http://localhost:8082/jugador/{dni}", Jugador.class, dni);
+		Jugador ju = restTemplate.getForObject("http://localhost:8022/jugador/{dni}", Jugador.class, dni);
 		model.addAttribute("jugador", ju);
 		return "viewUpdateJugador";
 
@@ -215,8 +216,57 @@ public class MainController {
 
 	@PostMapping ("pagina-update-jugador")
 	public String deleteJugador(Model model, @ModelAttribute Jugador ju){
-		restTemplate.put("http://localhost:8082/users", ju, Jugador.class);
+		restTemplate.put("http://localhost:8022/jugadores", ju, Jugador.class);
 		return "index";
 	}
+
+	/* CRUD CONTROLADOR - ADMINISTRADOR */
+	@GetMapping ("pagina-administrador/{nombre}")
+	public String returnAdministrador(Model model, @PathVariable String nombre) {
+
+		Administrador ad = restTemplate.getForObject("http://localhost:8021/admin/{nombre}", Administrador.class, nombre);
+		model.addAttribute("administrador", ad);
+		return "viewAdministrador";
+
+	}
+
+	@GetMapping ("pagina-todos-administradores")
+	public String returnTodosAdministradores(Model model) {
+		Administrador[] listaAd = restTemplate.getForObject("http://localhost:8021/administradores", Administrador[].class);
+		model.addAttribute("adminList", listaAd);
+		return "viewTodosAdministradores";
+	}
+
+
+	@PostMapping ("pagina-post-administrador")
+	public String saveAdmin(Model model, @ModelAttribute Administrador ad) {
+		Administrador newAdministrador = restTemplate.postForObject("http://localhost:8021/administradores", ad, Administrador.class);
+		model.addAttribute("administrador", newAdministrador);
+		return "viewAdministrador";
+	}
+
+	@PostMapping ("pagina-delete-administrador")
+	public String deleteAdmin(Model model, @RequestParam String adminName) {
+		Administrador delAdmin = restTemplate.getForObject("http://localhost:8021/admin/{nombre}", Administrador.class, adminName);
+		if (delAdmin != null) {
+			restTemplate.delete("http://localhost:8082/admin/{nombre}", delAdmin.getAdminCorreo());
+		}
+		return "index";
+	}
+
+	@PostMapping ("pagina-search-administrador")
+	public String searchAdmin(Model model, @RequestParam String nombre) {
+		Administrador ad = restTemplate.getForObject("http://localhost:8021/admin/{nombre}", Administrador.class, nombre);
+		model.addAttribute("administrador", ad);
+		return "viewUpdateAdministrador";
+
+	}
+
+	@PostMapping ("pagina-update-administrador")
+	public String deleteAdmin(Model model, @ModelAttribute Administrador ad){
+		restTemplate.put("http://localhost:8021/administradores", ad, Administrador.class);
+		return "index";
+	}
+
 
 }
